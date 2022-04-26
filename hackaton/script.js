4fbae6785fda2b5b208d6db3e99b9a41e9c55c8f
iftibar <agol.clinic@gmail.com>
2022-04-26 13:27:47 +0300

    python

--- hackaton/script.js
+++ hackaton/script.js
@@ -75,12 +75,14 @@ function creatCard() {
     document.getElementById('inputFacebook').textContent = document.getElementById('facebook').value;
     document.getElementById('inputAddress').textContent = document.getElementById('address').value;
     document.getElementById('inputSentence').textContent = '"' + document.getElementById('sentence').value + '"';
-
+    document.getElementById('inputWebsite').textContent= document.getElementById('website').value;
+    
     // create icon for website:
     // web = document.getElementById('website').value;
-    // // web.setAttribute("href", "web")
-    // document.getElementById('inputWebsite') = web;
-    // document.getElementById('inputWebsite').innerHTML = web;
+    // let a = document.createElement('a')
+    // a.setAttribute("href", web);
+    // document.getElementById('inputWebsite').appendChild(a);
+    // // document.card.appendChild(a);
     
 
 
@@ -100,16 +102,45 @@ let color1 = document.querySelector('.color1');
 let color2 = document.querySelector('.color2');
 let card = document.getElementById('card');
 
-function setColor() {
+function setBackgroundColor() {
     card.style.background = "linear-gradient(to right,  "
         + color1.value
         + ", "
         + color2.value
         + ")";
 }
-color1.addEventListener('input', setColor)
+color1.addEventListener('input', setBackgroundColor);
 
-color2.addEventListener('input', setColor)
+color2.addEventListener('input', setBackgroundColor);
+
+
+let color3 = document.querySelector('.color3');
+let color4 = document.querySelector('.color4');
+let left = document.getElementById("left");
+let right = document.getElementById("right");
+
+function setTextColor() {
+    left.style.color = color3.value;
+    right.style.color = color4.value;
+}
+color3.addEventListener("input", setTextColor);
+color4.addEventListener("input", setTextColor);
+
+let fontBtn = document.getElementById("fontBtn");
+let fonts = [ 'Cambria', 'Cochin', 'Georgia', 'Times', 'monospace', 'Franklin Gothic Medium', 'Arial Narrow', 'Arial', 'Lucida Sans', 'Lucida Grande', 'Lucida Sans Unicode', 'Verdana', 'sans-serif',  'Impact', 'Arial Narrow Bold'];
+let i = 0;
+function changeFont() {
+    console.log(fonts[i])
+    left.style.fontFamily = fonts[i]
+    right.style.fontFamily = fonts[i]
+    if (i < fonts.length -1){
+    i++;
+    }
+    else { i=0 }
+}
+
+
+fontBtn.addEventListener('click', changeFont)
 
 
 
